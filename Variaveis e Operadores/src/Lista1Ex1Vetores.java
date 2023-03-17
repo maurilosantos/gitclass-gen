003bf196aa9ac2934f023a3086e65235ffb7050f
maurilosantos <maurilosantoss1@gmail.com>
2023-03-17 18:13:54 -0300

    Exercícios Arrays

---
+++ Variaveis e Operadores/src/Lista1Ex1Vetores.java
@@ -0,0 +1,56 @@
+package pacoteJava;
+
+import java.util.Scanner;
+
+public class Lista1Ex1Vetores {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Dado um vetor contendo 10 números inteiros não ordenados e não repetidos,
+		 *  construa um algoritmo que consiga pesquisar dados no vetor, 
+		 *  onde o usuário irá digitar um número e o programa deve exibir na tela 
+		 *  a posição deste número no vetor. Caso o número não seja encontrado, 
+		 *  a mensagem: “Não foi encontrado!” deve ser exibida na tela.
+		 */
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int [] vetor = new int[10];
+		int num;
+		boolean encontrou = false;
+		
+		vetor[0] = 2;
+		vetor[1] = 5;
+		vetor[2] = 1;
+		vetor[3] = 3;		
+		vetor[4] = 4;
+		vetor[5] = 9;
+		vetor[6] = 7;
+		vetor[7] = 8;
+		vetor[8] = 10;
+		vetor[9] = 6;
+		
+		System.out.println("\nDigite o número que você deseja encontrar: ");
+		num = leia.nextInt();
+	
+		
+		
+		for (int i = 0; i < 10; i++) {
+			if (num == vetor[i]) {
+			System.out.println("\nO número "+num+" está localizado na posição: "+i);
+			encontrou = true;
+			break;  
+			}
+		}
+		
+		
+		
+		if (!encontrou) {
+	      System.out.println("Não foi encontrado!");
+	    }
+		
+		
+	}
+
+}
---
+++ Variaveis e Operadores/src/Lista2Ex03Matrizes.java
@@ -0,0 +1,106 @@
+package pacoteJava;
+
+import java.util.Scanner;
+
+public class Lista2Ex03Matrizes {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Elabore um algoritmo que leia uma Matriz 3x3 de números inteiros e em seguida, mostre na tela:
+			Todos os elementos da Diagonal Principal
+			Todos os elementos da Diagonal Secundária
+			A Soma de todos os elementos da Diagonal Principal
+			A Soma de todos os elementos da Diagonal Secundária
+			
+
+		 */
+			
+		
+			Scanner leia = new Scanner(System.in);
+		
+			int [][] matriz = new int[3][3];
+			int somaDiagonalP = 0, somaDiagonalS = 0;
+			int [] elementosP = new int[3];
+			int [] elementosS = new int[3];
+			
+			
+			System.out.println("\nDigite primeiro elemento: ");
+			matriz[0][0] = leia.nextInt();
+			
+			System.out.println("\nDigite segundo elemento: ");
+			matriz[0][1] = leia.nextInt();
+			
+			System.out.println("\nDigite terceiro elemento: ");
+			matriz[0][2] = leia.nextInt();
+			
+			System.out.println("\nDigite quarto elemento: ");
+			matriz[1][0] = leia.nextInt();
+			
+			System.out.println("\nDigite quinto elemento: ");
+			matriz[1][1] = leia.nextInt();
+			
+			System.out.println("\nDigite sexto elemento: ");
+			matriz[1][2] = leia.nextInt();
+			
+			System.out.println("\nDigite sétimo elemento: ");
+			matriz[2][0] = leia.nextInt();
+			
+			System.out.println("\nDigite oitavo elemento: ");
+			matriz[2][1] = leia.nextInt();
+			
+			System.out.println("\nDigite nono elemento: ");
+			matriz[2][2] = leia.nextInt();
+			
+			
+			
+			for(int linha=0;linha<3;linha++) {
+			
+				for(int coluna=0;coluna<3;coluna++) {
+	
+					if (linha == coluna) {
+					
+					 somaDiagonalP += matriz[linha][coluna];
+					 elementosP[linha] = matriz[linha][coluna];
+					
+					}
+				}
+			}
+			
+			for(int linha=0;linha<3;linha++) {
+				
+				for(int coluna=0;coluna<3;coluna++) {
+	
+					if (linha + coluna == matriz.length - 1) {
+					
+					 somaDiagonalS += matriz[linha][coluna];
+					 elementosS[linha] = matriz[linha][coluna];
+					
+					}
+				}
+			}
+			
+			
+			
+			
+			System.out.println("\nElementos da Diagonal Principal: ");
+		
+			for (int i = 0; i < elementosP.length; i++) {
+	            System.out.print(elementosP[i] + " ");
+	        }
+			
+			
+			System.out.println("\nElementos da Diagonal Secundária:: "+elementosS);
+			
+			for (int i = 0; i < elementosP.length; i++) {
+	            System.out.print(elementosS[i] + " ");
+	        }
+			
+			System.out.println("\nA Soma de todos os elementos da Diagonal Principal é: "+somaDiagonalP);
+			System.out.println("\nA Soma de todos os elementos da Diagonal Secundária é: "+somaDiagonalS);
+			
+			
+
+	}
+
+}
